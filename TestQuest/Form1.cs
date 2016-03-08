07331461567102cf10502316349ba66a2bce98de
maelstrom <alexandre.st-jacques@groupegodin.com>
2016-03-08 14:25:05 -0500

    Lol

--- TestQuest/Form1.cs
+++ TestQuest/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,16 +14,48 @@ namespace TestQuest
     public partial class Form1 : Form
     {
         Wheel wheel;
+        Thread t;
 
         public Form1()
         {
             InitializeComponent();
+
+            this.SetStyle(
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint |
+                ControlStyles.DoubleBuffer,
+                true);
+
             wheel = new Wheel();
+            t = new Thread(new ThreadStart(Run));
+            t.Start();
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
             wheel.Draw(e);
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    Invalidate();
+                    Thread.Sleep(10);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            t.Resume();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t.Suspend();
         }
     }
 }
