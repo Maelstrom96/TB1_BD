e883ec808d3b7d63b09c4e807f53e8844a380ea3
maelstrom <alexandre.st-jacques@groupegodin.com>
2016-03-15 15:07:55 -0400

    Fix Tab

--- TestQuest/AdminQuestions.Designer.cs
+++ TestQuest/AdminQuestions.Designer.cs
@@ -48,7 +48,7 @@
             this.BTN_Submit.Location = new System.Drawing.Point(287, 344);
             this.BTN_Submit.Name = "BTN_Submit";
             this.BTN_Submit.Size = new System.Drawing.Size(75, 23);
-            this.BTN_Submit.TabIndex = 0;
+            this.BTN_Submit.TabIndex = 11;
             this.BTN_Submit.Text = "Soumettre";
             this.BTN_Submit.UseVisualStyleBackColor = true;
             this.BTN_Submit.Click += new System.EventHandler(this.BTN_Submit_Click);
@@ -73,21 +73,21 @@
             this.TXT_Answer2.Location = new System.Drawing.Point(12, 207);
             this.TXT_Answer2.Name = "TXT_Answer2";
             this.TXT_Answer2.Size = new System.Drawing.Size(247, 20);
-            this.TXT_Answer2.TabIndex = 2;
+            this.TXT_Answer2.TabIndex = 3;
             // 
             // TXT_Answer3
             // 
             this.TXT_Answer3.Location = new System.Drawing.Point(12, 233);
             this.TXT_Answer3.Name = "TXT_Answer3";
             this.TXT_Answer3.Size = new System.Drawing.Size(247, 20);
-            this.TXT_Answer3.TabIndex = 2;
+            this.TXT_Answer3.TabIndex = 4;
             // 
             // TXT_Answer4
             // 
             this.TXT_Answer4.Location = new System.Drawing.Point(12, 259);
             this.TXT_Answer4.Name = "TXT_Answer4";
             this.TXT_Answer4.Size = new System.Drawing.Size(247, 20);
-            this.TXT_Answer4.TabIndex = 2;
+            this.TXT_Answer4.TabIndex = 5;
             // 
             // RB_Good1
             // 
@@ -95,7 +95,7 @@
             this.RB_Good1.Location = new System.Drawing.Point(265, 181);
             this.RB_Good1.Name = "RB_Good1";
             this.RB_Good1.Size = new System.Drawing.Size(97, 17);
-            this.RB_Good1.TabIndex = 3;
+            this.RB_Good1.TabIndex = 6;
             this.RB_Good1.TabStop = true;
             this.RB_Good1.Text = "Bonne réponse";
             this.RB_Good1.UseVisualStyleBackColor = true;
@@ -106,7 +106,7 @@
             this.RB_Good2.Location = new System.Drawing.Point(265, 207);
             this.RB_Good2.Name = "RB_Good2";
             this.RB_Good2.Size = new System.Drawing.Size(97, 17);
-            this.RB_Good2.TabIndex = 3;
+            this.RB_Good2.TabIndex = 7;
             this.RB_Good2.TabStop = true;
             this.RB_Good2.Text = "Bonne réponse";
             this.RB_Good2.UseVisualStyleBackColor = true;
@@ -117,7 +117,7 @@
             this.RB_Good3.Location = new System.Drawing.Point(265, 233);
             this.RB_Good3.Name = "RB_Good3";
             this.RB_Good3.Size = new System.Drawing.Size(97, 17);
-            this.RB_Good3.TabIndex = 3;
+            this.RB_Good3.TabIndex = 8;
             this.RB_Good3.TabStop = true;
             this.RB_Good3.Text = "Bonne réponse";
             this.RB_Good3.UseVisualStyleBackColor = true;
@@ -128,7 +128,7 @@
             this.RB_Good4.Location = new System.Drawing.Point(265, 256);
             this.RB_Good4.Name = "RB_Good4";
             this.RB_Good4.Size = new System.Drawing.Size(97, 17);
-            this.RB_Good4.TabIndex = 3;
+            this.RB_Good4.TabIndex = 9;
             this.RB_Good4.TabStop = true;
             this.RB_Good4.Text = "Bonne réponse";
             this.RB_Good4.UseVisualStyleBackColor = true;
@@ -139,7 +139,7 @@
             this.CB_Categories.Location = new System.Drawing.Point(141, 285);
             this.CB_Categories.Name = "CB_Categories";
             this.CB_Categories.Size = new System.Drawing.Size(118, 21);
-            this.CB_Categories.TabIndex = 4;
+            this.CB_Categories.TabIndex = 10;
             // 
             // label1
             // 
@@ -147,7 +147,7 @@
             this.label1.Location = new System.Drawing.Point(83, 288);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(52, 13);
-            this.label1.TabIndex = 5;
+            this.label1.TabIndex = 0;
             this.label1.Text = "Categorie";
             // 
             // BTN_Cancel
@@ -156,7 +156,7 @@
             this.BTN_Cancel.Location = new System.Drawing.Point(12, 344);
             this.BTN_Cancel.Name = "BTN_Cancel";
             this.BTN_Cancel.Size = new System.Drawing.Size(75, 23);
-            this.BTN_Cancel.TabIndex = 6;
+            this.BTN_Cancel.TabIndex = 12;
             this.BTN_Cancel.Text = "Annuler";
             this.BTN_Cancel.UseVisualStyleBackColor = true;
             this.BTN_Cancel.Click += new System.EventHandler(this.BTN_Cancel_Click);
