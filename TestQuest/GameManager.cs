1e88d65d824f9d60b9c8d7f2c792c2dcf867b794
Alexandre-Jacques St-Jacques <alexandre.st-jacques@groupegodin.com>
2016-03-18 12:53:57 -0400

    :P

--- TestQuest/GameManager.cs
+++ TestQuest/GameManager.cs
@@ -9,11 +9,23 @@ namespace TestQuest
     class GameManager
     {
         private Joueur[] joueurs;
+        private int currentPlayerIndex;
         private GestionQuestions gestionQuestions = new GestionQuestions();
 
-        public GameManager()
+        public GameManager(Joueur[] joueurs_)
         {
+            currentPlayerIndex = 1;
+            joueurs = joueurs_;
+        }
 
+        public Joueur GetCurrentPlayer()
+        {
+            return joueurs[currentPlayerIndex];
+        }
+
+        public Joueur[] GetPlayers()
+        {
+            return joueurs;
         }
     }
 }
